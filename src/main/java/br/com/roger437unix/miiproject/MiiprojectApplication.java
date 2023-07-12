5058fd6dfb0d21cdf0aff8ab044d97d29ef73258
roger437unix <roger437unix@gmail.com>
2023-07-12 14:18:11 -0300

    Project created

---
+++ src/main/java/br/com/roger437unix/miiproject/MiiprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.roger437unix.miiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiiprojectApplication.class, args);
+	}
+
+}
