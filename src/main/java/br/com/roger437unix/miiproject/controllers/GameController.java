675147936f054665f7b461a8bb03cc21ba28bd8e
Rogério Alcântara <roger437unix@gmail.com>
2023-07-16 22:20:45 -0300

    GET game by id

--- src/main/java/br/com/roger437unix/miiproject/controllers/GameController.java
+++ src/main/java/br/com/roger437unix/miiproject/controllers/GameController.java
@@ -4,22 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.roger437unix.miiproject.dto.GameDTO;
 import br.com.roger437unix.miiproject.dto.GameMinDTO;
 import br.com.roger437unix.miiproject.services.GameService;
 
 
-@RestController								// Configurar a classe para ser um Controller
-@RequestMapping(value = "/games")  			// Criando recurso [rota]
+@RestController											// Configurar a classe para ser um Controller
+@RequestMapping(value = "/games")  						// Criando recurso [rota]
 public class GameController {
 	
-	@Autowired			// Injetando a camada Service dentro da camada Controller
+	@Autowired											// Injetando a camada Service dentro da camada Controller
 	private GameService gameService;
 	
-	@GetMapping			// Mapeando com método Get do HTTP
-	public List<GameMinDTO> findAll() {
+	
+	@GetMapping(value = "/{id}")						// Mapeando com método Get do HTTP
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;		
+	}
+	
+	@GetMapping											// Mapeando com método Get do HTTP
+	public List<GameMinDTO> findAll() {	
 		List<GameMinDTO> result = gameService.findAll();
 		return result;		
 	}
--- src/main/java/br/com/roger437unix/miiproject/services/GameService.java
+++ src/main/java/br/com/roger437unix/miiproject/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.roger437unix.miiproject.dto.GameDTO;
 import br.com.roger437unix.miiproject.dto.GameMinDTO;
 import br.com.roger437unix.miiproject.entities.Game;
 import br.com.roger437unix.miiproject.repositories.GameRepository;
@@ -13,9 +15,17 @@ import br.com.roger437unix.miiproject.repositories.GameRepository;
 @Service						// Registrando um componente [Service] no Spring
 public class GameService {
 	
-	@Autowired				// Injetando a camada Repository dentro da camada Service
+	@Autowired					// Injetando a camada Repository dentro da camada Service
 	private GameRepository gameRepository;
+		
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;		
+	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto =result.stream().map(x -> new GameMinDTO(x)).toList();
